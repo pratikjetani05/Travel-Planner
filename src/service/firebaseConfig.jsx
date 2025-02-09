bb430b0611ee937d39ef0241d28e9af06f9ef2ae
pratikjetani05 <pratikjetani14@gmail.com>
2025-02-09 18:01:08 +0530

    minor bug fixed with env updated

--- src/service/firebaseConfig.jsx
+++ src/service/firebaseConfig.jsx
@@ -1,7 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getFirestore} from "firebase/firestore"
+import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -9,12 +9,12 @@ import { getFirestore} from "firebase/firestore"
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: "travel-ddf07.firebaseapp.com",
-  projectId: "travel-ddf07",
-  storageBucket: "travel-ddf07.firebasestorage.app",
-  messagingSenderId: "580074703075",
-  appId: "1:580074703075:web:32107669d853c03178ddd6",
-  measurementId: "G-5C8VDN9ZQQ",
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
